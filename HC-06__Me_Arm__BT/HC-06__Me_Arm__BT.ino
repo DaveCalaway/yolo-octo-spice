263c443171f3dae4fe930a925050fb4dbe62337f
Dave <davide.gariselli@gmail.com>
2015-06-25 21:05:55 +0200

    release candidate

--- HC-06__Me_Arm__BT/HC-06__Me_Arm__BT.ino
+++ HC-06__Me_Arm__BT/HC-06__Me_Arm__BT.ino
@@ -17,64 +17,62 @@ Nel caso non venga selezionato un altro motore, con 'j' e 'l' si incrementa/decr
   SoftwareSerial maestroSerial(10, 11); //maestro's communication ports 
 #endif
 
-MicroMaestro maestro(maestroSerial);
-
 char state=0;
 int motor=0;
-int passi=4000;
-int passi0=0;
-int passi1=0;
-int passi2=0;
+int passi=5500;
+int passi0=5500;
+int passi1=5500;
+int passi2=5500;
+int oldm=0;
+
+MicroMaestro maestro(maestroSerial);
 
 
 void setup() {
     maestroSerial.begin(9600);  // Default connection rate for Maestro module
+    maestro.setTarget(0, 5500);  // Posizione di riposo
     mySerial.begin(19200); // Default connection rate for my BT module    
+    
 }
 
 void loop() {
-  
   if(mySerial.available() > 0){
-      
+    
       state = mySerial.read();      
       
       if (state == 'a') 
         mySerial.println("Benveuto nel programma di prova MeArm del FabLab di Modena\n");
       
-      //if(state =='0' || state == '1' || state == '2'){
-                
+      if(state == '0' || state == '1' || state == '2' ){    
+        
+          mySerial.print("Hai selezionato il motore:");
+          mySerial.println(state);
+          motor=state;
           
- //ricordo vecchia posizione
-         // if(motor =! motorold){
-           
-         if(state == '0' ){
-             if(motor == state)  // il motor precedente era questo?
-                passi0=passi;
-             else
-                passi=passi0;
-         }
-         if(state == '1' ){
-             if(motor == state)  // il motor precedente era questo?
-                passi1=passi;
-             else
-                passi=passi1;
-         }
-         if(state == '2' ){
-             if(motor == state)  // il motor precedente era questo?
-                passi2=passi;
-             else
-                passi=passi2;
-         }
-         mySerial.print("Hai selezionato il motore:");
-         mySerial.println(state);
-         motor=state;
-        // }
+          if(motor == '0' && oldm != motor )  // passagli l'ultima posizione motore
+                 passi=passi0;   
+                 
+          if(state == '1' && oldm != motor )
+                 passi=passi1;
+          
+          if(state == '2' && oldm != motor)
+                 passi=passi2;
+          
       }
-      
+  
       if(state == 'j' || state == 'l'){
           mySerial.println(state);
-          Me_Arm(motor,state);
-       }  
+          Me_Arm(motor,state);        // chiamo funzione
+          
+          oldm=motor;         // salva posizione del motore
+          if(motor == '0')
+              passi0=passi;
+          if(motor=='1')
+              passi1=passi;
+          if(motor=='2')
+              passi2=passi;
+          
+      }  
+  }//serial.available
 }
-
 
--- HC-06__Me_Arm__BT/Me_Arm.ino
+++ HC-06__Me_Arm__BT/Me_Arm.ino
@@ -1,16 +1,23 @@
 // Conversione in pulsazioni per servo
 
 void Me_Arm(int motor,int target){
-    //stateold=target;
-    
+
     if( target == 'j' )
         passi=passi+400;
     else 
         passi=passi-400;
-  
+    mySerial.println(passi);
       
-    if( (passi > 4000) && (passi < 8000) ){
+    if( (passi >= 4000) && (passi <= 8000) ){
       maestro.setTarget( (motor-48), passi);
+      // visualizza posizione instantanea 
+      mySerial.print("motore 0: ");
+      mySerial.println(passi0);
+      mySerial.print("motore 1: ");
+      mySerial.println(passi1);
+      mySerial.print("motore 2: ");
+      mySerial.println(passi2);
+      
     }
     if(passi < 4000){  // fine corsa minimo
       mySerial.println("Fine corsa");
@@ -22,3 +29,5 @@ void Me_Arm(int motor,int target){
     }
     
 }
+
+
